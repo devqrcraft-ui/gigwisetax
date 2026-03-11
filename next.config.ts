0f4306217a56cc9ccb4fe96916cf529a27e08c8f
devqrcraft <devqrcraft@gmail.com>
2026-03-11 15:42:45 +0200

    perf: compress+WebP/AVIF images

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 31536000,
+  },
+};
 export default nextConfig;
